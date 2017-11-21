73894de9ed9a26f3c90578a61b070ef0d28f993f
cstornowski <christian.stornowski@diva-e.com>
2017-11-21 14:33:38 +0100

    *Added more tests

--- src/test/java/com/divae/firstspirit/access/store/templatestore/gom/GomFormElementMockTest.java
+++ src/test/java/com/divae/firstspirit/access/store/templatestore/gom/GomFormElementMockTest.java
@@ -21,6 +21,4 @@ public class GomFormElementMockTest extends MockTest {
         GomFormElement gomFormElement = build(gomFormElementWith("test"));
         assertThat(gomFormElement.name(), is("test"));
     }
-
-
 }
